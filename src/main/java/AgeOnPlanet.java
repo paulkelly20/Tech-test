23b7db49d4e6fc0a4a21c4102b6eec238dcf5799
Paul Kelly <paul.kelly20@gmail.com>
2018-05-22 16:51:05 +0100

    test for getting earth age passes

--- src/main/java/AgeOnPlanet.java
+++ src/main/java/AgeOnPlanet.java
@@ -1,5 +1,10 @@
 public class AgeOnPlanet {
 
 
+    public static double getEarthAgeInYearsFromSeconds(int seconds){
+        double result = (seconds / 31557600);
+        return result;
+    }
+
 
 }
