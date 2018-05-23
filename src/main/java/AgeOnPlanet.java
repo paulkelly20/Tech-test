e8706be24b0df077d406874b3cf0b9c6a54a6b30
Paul Kelly <paul.kelly20@gmail.com>
2018-05-23 09:11:52 +0100

    added math.floor method

--- src/main/java/AgeOnPlanet.java
+++ src/main/java/AgeOnPlanet.java
@@ -3,7 +3,7 @@ public class AgeOnPlanet {
 
     public static double getEarthAgeInYearsFromSeconds(int seconds){
         double result = (seconds / 31557600);
-        return result;
+        return Math.floor(result);
 
     }
 
